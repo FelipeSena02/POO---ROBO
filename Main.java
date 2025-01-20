65c6f371e190c2669dd86994bfa1caa839c6afa3
Felipe Sena <133073284+FelipeSena02@users.noreply.github.com>
2025-01-20 16:44:44 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package robo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Robo robo = new Robo("Azul");
+
+        System.out.println("Digite a posição do alimento (x y): ");
+        int comidaX = scanner.nextInt();
+        int comidaY = scanner.nextInt();
+
+        while (!robo.encontrouAlimento(comidaX, comidaY)) {
+            System.out.println("Movimente o robô (up/down/right/left): ");
+            String movimento = scanner.next();
+            try {
+                robo.mover(movimento);
+            } catch (MovimentoInvalidoException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        System.out.println("O robô encontrou o alimento!");
+    }
+}
---
+++ MovimentoInvalidoException.java
@@ -0,0 +1,7 @@
+package robo;
+
+public class MovimentoInvalidoException extends Exception {
+    public MovimentoInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+}
